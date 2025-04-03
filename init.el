a09b5cebc20436707eb7caab1f4d48f8c5e25dcd
sarrus1811 <ismail.abdulla.k@gmail.com>
2025-04-03 21:51:40 +0800

    Added init.el

---
+++ init.el
@@ -0,0 +1,75 @@
+(setq inhibit-startup-message t)
+
+
+;; UI settings
+(global-display-line-numbers-mode 1) ; Display line numbers
+
+(scroll-bar-mode -1)        ; Disable scrollbar
+(tool-bar-mode -1)          ; Disable toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)          ; Disable menu bar
+
+;; Enable visible bell
+(setq visible-bell t)
+
+;; Font
+(set-face-attribute 'default nil :font "JetBrains Mono" :height 110)
+
+;; Theme
+(load-theme 'tango-dark)
+
+
+;; Initialize package sources
+(require 'package)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+ (package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+   (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; For trouble shooting
+;; (use-package command-log-mode)
+
+;; ivy config
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(doom-modeline command-log-mode use-package ivy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Doom mode line settings
